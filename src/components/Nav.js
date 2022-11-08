a008a24cd3b139abda10759f1a110487c08fcff2
FranBaca <franbaca13@gmai.com>
2022-11-08 11:55:44 -0300

    Add Navbar component, and searchbox functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useDispatch } from 'react-redux';
+import { fetchCharacters } from '../actions';
 
 export default function NavbarComponent() {
+  const [name, setName]=useState("")
+  const dispatch = useDispatch()
+
+
+  function handleChange (event){
+      setName(event.target.value)
+  }
+
+  function handleSubmit(event){
+      console.log(name)
+      event.preventDefault()
+      dispatch(fetchCharacters(name))
+  }
+ 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
@@ -14,6 +31,18 @@ export default function NavbarComponent() {
           <Nav className="me-auto">
             <Nav.Link href="#home"  bg="light">Characters</Nav.Link>
             <Nav.Link href="#link">Planets</Nav.Link>
+            <form className="d-flex"  onSubmit={handleSubmit}>
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              value={name}
+              onChange={handleChange}
+            />
+            <Button variant="secondary" type="submit">Search</Button>
+            </form>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
