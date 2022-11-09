ed9d0a7583d12bd8550e3a39988f5a320065edd7
FranBaca <franbaca13@gmai.com>
2022-11-09 13:09:57 -0300

    Add react-redux logic into component characters list

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -2,57 +2,64 @@ import React, { useState } from 'react'
 import { useEffect } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from "react-bootstrap/Container"
+import {fetchCharacters} from "../actions/index"
+import { useDispatch, useSelector } from 'react-redux';
 import "./CharacterList.css"
+import { Link } from 'react-router-dom';
+
+
+
 export default function CharactersList() {
-  const [characters,setCharacters] = useState([])
-  let [page,setPage]  =useState(1)
-  async function fetchCharacters(){
-    let res = await fetch(`https://swapi.dev/api/people/?page=${page}`)
-    let data = await res.json()
-   setCharacters(data.results);
-  }
+  const chars = useSelector(state => state.characters)
+  let [page, setPage] = useState(0)
+ const dispatch = useDispatch()
+
+ useEffect(()=>{
+     dispatch(fetchCharacters())
+ },[dispatch])
   
-  const handleClickNext=()=>{
-        
-    if(page+1 < 36){
-        setPage(page+=1)
-        fetchCharacters(page+=1)
-    }
+ const handleClickNext=()=>{   
+  if(page+1 < 9){
+      setPage(page+=1)
+      dispatch(fetchCharacters(null,page+=1))
+  }
 }
 
 const handleClickBack=()=>{
-    if(page-1 >= 0){
-        setPage(page-1)
-       fetchCharacters(page-1)
-    }
+  if(page-1 >= 0){
+      setPage(page-1)
+      dispatch(fetchCharacters(null,page-1))
+  }
 }
+
   useEffect(() => {
     fetchCharacters()
   }, [])
-  console.log(characters)
+
   return (
-    <div className='charactersContainer'>
-   { characters?.map( c =>{
-      return(
-      <Card style={{ width: '15rem' }}>
+    <div className='charListContainer'>
+    <div className='charContainer'>
+   { chars?.results?.map( (c,index) =>{
+      return( <Link to={`/charDetail/${index+1}`}>
+        <Card bg="dark" className="Card" key={c.name} id={c.name} style={{ width: '15rem', marginBottom:"20px" }}>
       <Card.Body>
         <Card.Title>{c.name}</Card.Title>
         <Card.Text>Birth: {c.birth_year} </Card.Text>
         <Card.Text>Gender: {c.gender} </Card.Text>
-        <Card.Text>Height: {c.species} </Card.Text>
-        <Button variant="primary">Learn more</Button>
+        <Card.Text>Height: {c.height} </Card.Text>
+        <Button variant="secondary">See more</Button>               
       </Card.Body>
     </Card>
+    </Link>
+    
       )
     })}
-
-<div style={{marginTop:"35px" }}>
-                <button style={{backgroundColor:"#ADCBB6", borderRadius:"25px",cursor:"pointer"}} onClick={handleClickBack}> go</button>
-                <button style={{backgroundColor:"#8AAABC", borderRadius:"25px",cursor:"pointer"}} onClick={handleClickNext} >next </button>
-            </div>
    </div>
+
+  <div style={{margin:"10px"}}>
+                <Button variant="secondary" onClick={handleClickBack}> Back</Button>
+                <Button variant="secondary" onClick={handleClickNext} >Next </Button>
+            </div>
+      </div>
   )
 }
