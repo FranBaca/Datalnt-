8978dadde311c059303e7cdadb548a099508cebd
FranBaca <franbaca13@gmai.com>
2022-12-07 14:52:39 -0300

    Limpiar declaraciones no usadadas y agregue archivo .env

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import Nav from "./components/Nav.js"
 import { Route,Routes } from "react-router-dom";
-import CharactersList from "./components/CharactersList.js";
-import CharacterCard from "./components/CharacterCard.js";
+import CharactersList from "./components/HomePage/HomePage";
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
 function App(props) {
   return (
     <div className="App">
-      <Nav/>
       <Routes>
     <Route path="/" element={<CharactersList/>} />
     <Route path="/charDetail/:id" {...props} element={<CharacterCard/>} />
